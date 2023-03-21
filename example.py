a32c4f57f235aa71c78547d4f807ce1898c6f93b
ThomasDecorteUA <114341064+ThomasDecorteUA@users.noreply.github.com>
2023-03-21 21:26:01 +0100

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ import pandas as pd
 import statsmodels.api as sm
 import lightgbm as lgb
 import argparse
-import logging
 
 def init_arg():
     parser = argparse.ArgumentParser()
@@ -18,18 +17,15 @@ if __name__ == '__main__':
     df = pd.read_csv(r'house_KC_data.csv')
     df = sm.tools.tools.add_constant(df, prepend=True, has_constant='skip')
     features = ['intercept', 'bedrooms', 'bathrooms', 'floors', 'waterfront', 'view', 'condition', 'grade',
-                'log_sqft_living', 'log_sqft_lot', 'log_sqft_above',
-                'log_sqft_living15', 'log_sqft_lot15',
                 'age', 'age_rnv']
     target_variable = "target_log"
     light_gbm = lgb.LGBMRegressor()
     light_gbm.fit(df[features], df[target_variable])
     pred_train = light_gbm.predict(df[features])
     res_train = pred_train - df[target_variable]
-    beta,post_hoc_cost,initial_cost = cost_sens_boost.IRLS(args.type,df[features],res_train,args.a,args.b)
-    print("The below costs and One-Step Boosting algorithm is run using cost function",args.type, "with costs:"
+    beta,post_hoc_cost,initial_cost = cost_sens_boost.IRLS(args.type,df[features],features,res_train,args.a,args.b)
+    print("The below costs and One-Step Boosting (OSB) algorithm is run using cost function",args.type, "with costs:"
           , args.a,"/",args.b)
     print("Initial average misprediction cost: ", initial_cost)
     print("Coefficients of boosting step: ", beta)
     print("Post-hoc average misprediction cost: ", post_hoc_cost)
-
